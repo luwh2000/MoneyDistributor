56343374072a30a2480dbdf89b15d1bee2daee34
LowenSunday <luwh2000@gmail.com>
2019-11-03 22:44:37 -0500

    Add another person in the house

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        double[] a = new double[4];
-        System.out.println("焦子腾:");
+        double[] a = new double[5];
+        System.out.println("A房房主:");
         a[0] = scanner.nextDouble();
-        System.out.println("Richard:");
+        System.out.println("B房房主:");
         a[1] = scanner.nextDouble();
-        System.out.println("William:");
+        System.out.println("C房房主:");
         a[2] = scanner.nextDouble();
-        System.out.println("卢文昊:");
+        System.out.println("D房房主:");
         a[3]  = scanner.nextDouble();
-        double sum = a[0] + a[1] + a[2] + a[3];
-        double each = sum / 4;
+        System.out.println("房客");
+        a[4] = scanner.nextDouble();
+        double sum = a[0] + a[1] + a[2] + a[3] + a[4];
+        System.out.println("总共花了:" + sum);
+        double each = sum / 4.5;
+        System.out.println("每个房主花了:" + each);
+        System.out.println("房客花了:" + each * 0.5);
         for (int i = 0; i < 4; i++) {
             a[i] = a[i] - each;
         }
-        System.out.println("焦子腾:" + a[0] + "\nRichard:" + a[1] + "\nWilliam:" + a[2] + "\n卢文昊:" + a[3]);
+        a[4] = a[4] - each * 0.5;
+        System.out.println("A房房主:" + a[0] + "\nB房房主:" + a[1] + "\nC房房主:" + a[2] + "\nD房房主:" + a[3] + "\n房客:" + a[4]);
     }
 }
